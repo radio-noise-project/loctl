4f904cff7a438a7a7d972eb4b7422dc31ed72960
Aoi Kondo <agotadmidori@gmail.com>
2024-11-26 22:15:27 +0900

    feat: Add a fucntion to init a config file

---
+++ internal/config/init_test.go
@@ -0,0 +1,12 @@
+package config
+
+import "testing"
+
+func TestInitializeConfiguration(t *testing.T) {
+	host := "localhost"
+	port := 8080
+	err := InitializeConfiguration(host, port)
+	if err != nil {
+		t.Fatalf("excepted no error; but got %v", err)
+	}
+}
