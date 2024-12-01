7ec8c55ed6f175daf185720f55407c6667916c65
Aoi Kondo <agotadmidori@gmail.com>
2024-12-01 19:34:29 +0900

    feat: Create init command

---
+++ cmd/loctl/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/radio-noise-project/loctl/cli/command"
+
+func main() {
+	command.Execute()
+}
--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/radio-noise-project/loctl/cmd"
-
-func main() {
-	cmd.Execute()
-}
