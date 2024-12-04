30a4eeb79e9827c1b2c8a37f62dba487e83078ae
Aoi Kondo <agotadmidori@gmail.com>
2024-12-04 19:37:35 +0900

    feat: Add the run command

--- cli/run/run.go
+++ cli/run/run.go
@@ -2,6 +2,7 @@ package run
 
 import (
 	"github.com/radio-noise-project/loctl/cli"
+	"github.com/radio-noise-project/loctl/pkg/run"
 	"github.com/spf13/cobra"
 )
 
@@ -9,7 +10,9 @@ var runCmd = &cobra.Command{
 	Use:   "run",
 	Short: "Run code on Sisters",
 	Long:  ``,
+	Args:  cobra.ExactArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
+		run.StartContainer(args[0], args[1])
 	},
 }
 
---
+++ pkg/run/run.go
@@ -0,0 +1,87 @@
+package run
+
+import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/radio-noise-project/loctl/internal/client"
+)
+
+func StartContainer(sisterId string, dirname string) {
+	cli := client.NewConfiguration()
+	sendTarball(dirname, cli)
+}
+
+func sendTarball(dirname string, cli *client.Config) {
+	var buf bytes.Buffer
+	var sendBuf bytes.Buffer
+	gzw := gzip.NewWriter(&buf)
+	tw := tar.NewWriter(gzw)
+
+	err := filepath.Walk(dirname, func(file string, fi os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		header, err := tar.FileInfoHeader(fi, file)
+		if err != nil {
+			return err
+		}
+
+		header.Name = filepath.ToSlash(file)
+
+		if err := tw.WriteHeader(header); err != nil {
+			return err
+		}
+
+		if fi.Mode().IsRegular() {
+			f, err := os.Open(file)
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+
+			if _, err := io.Copy(tw, f); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := tw.Close(); err != nil {
+		panic(err)
+	}
+
+	if err := gzw.Close(); err != nil {
+		panic(err)
+	}
+
+	writer := multipart.NewWriter(&sendBuf)
+	filewriter, err := writer.CreateFormFile("file", "test.tar.gz")
+	if err != nil {
+		panic(err)
+	}
+
+	io.Copy(filewriter, &buf)
+	writer.Close()
+
+	resp, err := http.Post(cli.DestinationUrl+"/api/v0/run", writer.FormDataContentType(), &sendBuf)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		panic("failed to upload tarball")
+	}
+}
